32923d0bbc33073e9a8efb6e23f763c721b0c9ba
Elton Ruan da Silva Santos <124888687+EltonRuan@users.noreply.github.com>
2024-11-08 16:25:32 -0300

    no comments

--- app/Http/Controllers/UsuarioController.php
+++ app/Http/Controllers/UsuarioController.php
@@ -10,24 +10,20 @@ class UsuarioController extends Controller
 {
     public function cadastrar(Request $request)
     {
-        // Validação dos dados de entrada
         $request->validate([
             'nome' => 'required|string|max:255',
             'email' => 'required|string|email|max:255|unique:usuarios',
-            'senha' => 'required|string|min:6|confirmed', // 'confirmed' valida se 'senha' e 'senha_confirmation' são iguais
+            'senha' => 'required|string|min:6|confirmed',
         ]);
     
-        // Criar o usuário, criptografando a senha diretamente aqui
         $user = User::create([
             'nome' => $request->nome,
             'email' => $request->email,
-            'senha' => Hash::make($request->senha), // Criptografando a senha diretamente no controlador
+            'senha' => Hash::make($request->senha),
         ]);
     
-        // Gerar o token de autenticação
         $token = $user->createToken('PersonalAccessToken')->plainTextToken;
     
-        // Retornar os dados do usuário e o token
         return response()->json([
             'usuario' => $user,
             'token' => $token,
@@ -36,7 +32,6 @@ class UsuarioController extends Controller
 
     public function login(Request $request)
     {
-        // Validação de login
         $request->validate([
             'email' => 'required|email',
             'senha' => 'required',
@@ -48,10 +43,8 @@ class UsuarioController extends Controller
             return response()->json(['message' => 'Credenciais inválidas.'], 401);
         }
 
-        // Gerar o token de autenticação
         $token = $user->createToken('Token de Acesso')->plainTextToken;
 
-        // Salvar o token no usuário
         $user->token = $token;
         $user->save();
 
@@ -63,12 +56,10 @@ class UsuarioController extends Controller
 
     public function logout(Request $request)
     {
-        // Deletar todos os tokens do usuário
         $request->user()->tokens->each(function ($token) {
             $token->delete();
         });
 
-        // Atualizar o campo de token do usuário
         $request->user()->update(['token' => null]);
 
         return response()->json(['message' => 'Logout bem-sucedido']);
